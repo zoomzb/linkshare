418519979b980fafc078676a86ef2889e3cb5e5e
zoomzb <2456066869@qq.com>
2021-07-16 12:00:20 +0800

    new update

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,11 +1,115 @@
 <template>
     <div class="main-container">
 		<div class="left">
-			<div>
-				<h1></h1>
-				
+			<div class="res-item">
+				<div class="hover-move">
+					<div class="res-main">
+						<div class="res-img">
+							<img src="https://linkfind.cn/api/image/1625552806140.png">
+						</div>
+						<div class="res-content">
+							<div class="res-content-title">PALX</div>
+							<div class="res-content-desc">免费！自动 UI 调色板生成器，只需要提供一个主色值，即可根据算法返回需要的整套色板，非常的方便快捷，快来试试吧</div>
+							<div class="res-content-footer">
+								<div class="res-url">palx.jxnblk.com</div>
+								<div class="res-tag">
+									<div class="res-tag-content">PLAX</div>
+									<div class="res-tag-content">PLAX</div>
+									<div class="res-tag-content">PLAX</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 
+			<div class="res-item">
+				<div class="hover-move">
+					<div class="res-main">
+						<div class="res-img">
+							<img src="https://linkfind.cn/api/image/1625552806140.png">
+						</div>
+						<div class="res-content">
+							<div class="res-content-title">PALX</div>
+							<div class="res-content-desc">免费！自动 UI 调色板生成器，只需要提供一个主色值，即可根据算法返回需要的整套色板，非常的方便快捷，快来试试吧</div>
+							<div class="res-content-footer">
+								<div class="res-url">palx.jxnblk.com</div>
+								<div class="res-tag">
+									<div class="res-tag-content">PLAX</div>
+									<div class="res-tag-content">PLAX</div>
+									<div class="res-tag-content">PLAX</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="res-item">
+				<div class="hover-move">
+					<div class="res-main">
+						<div class="res-img">
+							<img src="https://linkfind.cn/api/image/1625552806140.png">
+						</div>
+						<div class="res-content">
+							<div class="res-content-title">PALX</div>
+							<div class="res-content-desc">免费！自动 UI 调色板生成器，只需要提供一个主色值，即可根据算法返回需要的整套色板，非常的方便快捷，快来试试吧</div>
+							<div class="res-content-footer">
+								<div class="res-url">palx.jxnblk.com</div>
+								<div class="res-tag">
+									<div class="res-tag-content">PLAX</div>
+									<div class="res-tag-content">PLAX</div>
+									<div class="res-tag-content">PLAX</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="res-item">
+				<div class="hover-move">
+					<div class="res-main">
+						<div class="res-img">
+							<img src="https://linkfind.cn/api/image/1625552806140.png">
+						</div>
+						<div class="res-content">
+							<div class="res-content-title">PALX</div>
+							<div class="res-content-desc">免费！自动 UI 调色板生成器，只需要提供一个主色值，即可根据算法返回需要的整套色板，非常的方便快捷，快来试试吧</div>
+							<div class="res-content-footer">
+								<div class="res-url">palx.jxnblk.com</div>
+								<div class="res-tag">
+									<div class="res-tag-content">PLAX</div>
+									<div class="res-tag-content">PLAX</div>
+									<div class="res-tag-content">PLAX</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="res-item">
+				<div class="hover-move">
+					<div class="res-main">
+						<div class="res-img">
+							<img src="https://linkfind.cn/api/image/1625552806140.png">
+						</div>
+						<div class="res-content">
+							<div class="res-content-title">PALX</div>
+							<div class="res-content-desc">免费！自动 UI 调色板生成器，只需要提供一个主色值，即可根据算法返回需要的整套色板，非常的方便快捷，快来试试吧</div>
+							<div class="res-content-footer">
+								<div class="res-url">palx.jxnblk.com</div>
+								<div class="res-tag">
+									<div class="res-tag-content">PLAX</div>
+									<div class="res-tag-content">PLAX</div>
+									<div class="res-tag-content">PLAX</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 		<div class="right">
 			<ul>
@@ -21,8 +125,6 @@
 				</li>
 			</ul>
 		</div>
-
-
     </div>
 </template>
 <script>
@@ -75,17 +177,109 @@ export default {
 </script>
 <style lang="less">
 	.main-container {
+		display: flex;
+		flex-direction: row;
 		margin-top: 150px;
-		max-width: 1300px;
+		max-width: 1500px;
 		margin: 150px auto 90px;
 		overflow: hidden;
 		box-sizing: border-box;
 		.left {
-			float: left;
+			// float: left;
+			display: flex;
+			flex: 1;
+			flex-wrap: wrap;
 			width: 980px;
+			.res-item {
+				width: 25%;
+				display: flex;
+				padding: 20px;
+				.hover-move {
+					width: 100%;
+					.res-main {
+						width: 100%;
+						padding: 20px 25px;
+						background-color: #f5f5f5;
+						border-radius: 5px;
+						cursor: pointer;
+						position: relative;
+						transition: all .3s;
+						.res-img {
+							width: 70px;
+							height: 70px;
+							border-radius: 50%;
+							overflow: hidden;
+							position: relative;
+							font-size: 20px;
+							line-height: 70px;
+							img {
+								position: absolute;
+								top: 0;
+								left: 0;
+								user-select: none;
+								width: 100%;
+							}
+						}
+						.res-content {
+							.res-content-title {
+								font-weight: 800;
+								font-size: 20px;
+								overflow: hidden;
+								text-overflow: ellipsis;
+								white-space: nowrap;
+								color: black;
+							}
+							.res-content-desc {
+								font-size: 10px;
+								margin-top: 10px;
+								height: 38px;
+								word-break: break-all;
+								text-overflow: ellipsis;
+								// white-space: nowrap;
+								overflow: hidden;
+								display: -webkit-box;
+								-webkit-line-clamp: 2;
+								-webkit-box-orient: vertical;
+							}
+							.res-content-footer {
+								font-size: 8px;
+								.res-url {
+									width: fit-content;
+									overflow: hidden;
+									font-weight: 500;
+									text-overflow: ellipsis;
+									white-space: nowrap;
+									cursor: pointer;
+									margin-top: 10px;
+									height: 15px;
+								}
+								.res-tag {
+									overflow: hidden;
+									white-space: nowrap;
+									text-overflow: ellipsis;
+									display: flex;
+									margin-top: 10px;
+									.res-tag-content:not(:first-child){
+										margin-left: 5px;
+									}
+									.res-tag-content {
+										padding: 0 10px;
+										cursor: pointer;
+										background-color: #e4dfdf;
+										color: black;
+										border-radius: 2px;
+										
+									}
+								}
+							}
+						}
+					}
+				}
+			}
 		}
 		.right {
-			float: right;
+			position: flex;
+			margin-top: 20px;
 			width: 20%;
 			background-color: #ff5722;
 			color: #ffffff;
